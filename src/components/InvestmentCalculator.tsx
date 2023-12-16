161996b623b8c1a9d65fb920f901185dd4a9d94c
Debashis Roy <goodquote39@hotmail.com>
2023-12-16 22:54:36 +0600

    investment calculator

--- src/components/InvestmentCalculator.tsx
+++ src/components/InvestmentCalculator.tsx
@@ -1,9 +1,58 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
 import React from "react";
+import Slider from "./ui/Slider";
 
 const InvestmentCalculator: React.FC = () => {
+  const handleSliderChange = (value: number) => {
+    console.log("Slider value changed:", value);
+    // Handle the value change as needed
+  };
   return (
     <div className="mt-20 mb-10">
-      <p>investment calculator</p>
+      <p className="text-[#EE7214] text-[15px] uppercase">
+        WE are changing the way you invest
+      </p>
+      <h3 className="text-[36px] font-semibold">Investment Calculator</h3>
+      <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
+        <div className="bg-[#F2F2F2]  border border-[#DCDCDC] rounded-md px-5 py-10 text-justify">
+          <h4 className="text-[20px] font-semibold">
+            How much do you want to invest?
+          </h4>
+          <Slider
+            content="Initial Investment"
+            minValue={10000}
+            maxValue={50000}
+            price
+            onChange={handleSliderChange}
+          />
+          <Slider
+            content="Property Value Growth(over 5 year)"
+            minValue={0}
+            maxValue={100}
+            percent
+            onChange={handleSliderChange}
+          />
+          <Slider
+            content="Expected annual rental yield"
+            minValue={0}
+            maxValue={100}
+            percent
+            onChange={handleSliderChange}
+          />
+          <p className="mt-5 text-[12px]">
+            All projected values are before any property costs and platform
+            fees, and based on a 5-year holding period. We expect the asset
+            value to grow 30% over the next 5 years.*
+          </p>
+        </div>
+        <div>
+          <img
+            src="/images/chart.png"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/ui/Slider.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React, { useState, ChangeEvent } from "react";
+
+interface SliderProps {
+  initialValue?: number;
+  minValue?: number;
+  maxValue?: number;
+  className?: string;
+  content?: string;
+  price?: boolean;
+  percent?: boolean;
+  onChange: (value: number) => void;
+}
+
+const Slider: React.FC<SliderProps> = ({
+  initialValue = 50,
+  minValue = 0,
+  maxValue = 100,
+  onChange,
+  className,
+  content,
+  price,
+  percent,
+}) => {
+  const [value, setValue] = useState<number>(initialValue);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(event.target.value);
+    setValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <div className={`${className} w-full mx-auto mt-8`}>
+      <div className="flex justify-between items-center">
+        <p>{content}</p>
+        <div className="mt-2 text-lg">
+          {price && "$"}
+          {value}
+          {percent && "%"}
+        </div>
+      </div>
+      <input
+        type="range"
+        min={minValue}
+        max={maxValue}
+        value={value}
+        className="w-full h-2 rounded bg-gray-300 appearance-none focus:outline-none focus:shadow-outline"
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Slider;
