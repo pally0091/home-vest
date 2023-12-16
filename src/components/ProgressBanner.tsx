e8599f8f410de6ff6ded742609d93882e4a1fd52
Debashis Roy <goodquote39@hotmail.com>
2023-12-16 23:51:16 +0600

    progress banner working

--- src/components/ProgressBanner.tsx
+++ src/components/ProgressBanner.tsx
@@ -3,7 +3,16 @@ import React from "react";
 const ProgressBanner: React.FC = () => {
   return (
     <div className="mt-20 mb-10">
-      <p>Progress Banner</p>
+      <p className="uppercase text-[15px] text-[#ee7214]">
+        WE are changing the way you invest
+      </p>
+      <h3 className="text-[36px] font-semibold">It’s easy to get started.</h3>
+      <p className="text-[15px] text-[#3f3f3f] my-5 w-[90%] md:w-[80%] lg:w-[60%] mx-auto">
+        We specialize in investing in single family rental homes in key markets
+        in the American South East. Our platform allows investors to purchase
+        SEC-compliant shares in single family homes for as little as $100.
+      </p>
+      <div>progress banner</div>
     </div>
   );
 };
